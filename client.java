e86a168d32e15735fe9c6b494ef33523964a6c64
andreology <37252145+andreology@users.noreply.github.com>
2019-11-15 20:40:13 -0800

    adding port and ip input feature

--- client.java
+++ client.java
@@ -6,10 +6,18 @@ import java.io.*;
 
 public class client {
 	public static void main(String args[]) throws Exception{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please enter an ip address");
+		String ipInput = in.next();
+		System.out.println("Please enter an port address");
+		int portInput = in.nextInt();
+	
+		
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Please Enter a Message to Send to Server?");
 		DatagramSocket clientSocket = new DatagramSocket();
-		InetAddress ip = InetAddress.getByName("localhost");
+		//InetAddress ip = InetAddress.getByName("localhost");
+		InetAddress ip = InetAddress.getByName(ipInput);
 		byte inData[] = new byte[1024];
 		byte outData[] = new byte[1024];
 		String data = input.readLine();
@@ -18,12 +26,12 @@ public class client {
 		
 
 		
-		
+		//port 1234
 				
 		outData = data.getBytes();
-		DatagramPacket packet = new DatagramPacket(outData, outData.length, ip, 1234);
+		DatagramPacket packet = new DatagramPacket(outData, outData.length, ip, portInput);
 		clientSocket.send(packet);
-		DatagramPacket packet2 = new DatagramPacket(inData, inData.length, ip, 1234);
+		DatagramPacket packet2 = new DatagramPacket(inData, inData.length, ip, portInput);
 		clientSocket.receive(packet2);
 		String receiveData =new String(packet2.getData());
 		System.out.println(receiveData);
